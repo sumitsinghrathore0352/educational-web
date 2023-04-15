b6d65e3edb81a982f417074d904ee5203ff2c5ce
sumitsinghrathore0352 <sumitsinghrajput@gamil.com>
2023-04-15 10:55:00 +0530

    deployed

---
+++ JS/resp.js
@@ -0,0 +1,14 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+// navlist= document.querySelector('.nav-list')
+
+
+    burger.addEventListener('click',()=>{
+        rightnav.classList.toggle('v-class-resp');
+        navlist.classList.toggle('v-class-resp');
+        navbar.classList.toggle('h-class-resp');
+        
+
+    })
